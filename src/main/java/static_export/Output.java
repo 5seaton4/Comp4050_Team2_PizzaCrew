c8dea264803ff275b1406155a0181a067a5599d5
Abhi <abhinavmreddy345@gmail.com>
2022-10-11 02:16:54 +1100

    PIZ-35: class created to read cmd output

---
+++ src/main/java/static_export/Output.java
@@ -0,0 +1,17 @@
+package static_export;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
+public class Output {
+    public static void main(String args[]) throws FileNotFoundException {
+        File file = new File("C:/Users/abhin/Documents/COMP4050Pizza/Comp4050_Team2_PizzaCrew/data1.txt");
+        Scanner scan = new Scanner(file);
+
+        while(scan.hasNextLine()) {
+            System.out.println("Hi");
+            System.out.println(scan.nextLine());
+        }
+    }
+}
